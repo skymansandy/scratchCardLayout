ae66e73acb5c40e75d92e9bc4619dc7489b1a501
P Sandesh Baliga <sandeshbaliga96@gmail.com>
2019-03-29 19:43:11 +0530

    Initial commit

---
+++ scratchcardview/src/main/java/in/codeshuffle/scratchcardview/ScratchCardView.java
@@ -0,0 +1,19 @@
+package in.codeshuffle.scratchcardview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class ScratchCardView extends RelativeLayout {
+    public ScratchCardView(Context context) {
+        super(context);
+    }
+
+    public ScratchCardView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ScratchCardView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
